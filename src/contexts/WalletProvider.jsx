5505a55b06990d17ef10035cfd31cb531f4d4920
jwu-ice <sju02048@naver.com>
2022-05-18 23:35:03 +0900

    :recycle: Provider 3개로 나눔

--- src/contexts/WalletProvider.jsx
+++ src/contexts/WalletProvider.jsx
@@ -1,42 +1,18 @@
-import React, { createContext, useCallback, useEffect, useMemo, useReducer, useState } from "react";
+import React, { createContext, useCallback, useEffect, useMemo, useReducer } from "react";
 import moneyData from "mocks/moneyData";
-import { calcTotalMoney } from "helpers/calculateMoney";
-import { MY_MONEY } from "constants/myWallet";
-
-const example = {
-  wallet: [
-    {
-      id: 1,
-      type: "coin",
-      money: 10,
-      count: 0,
-    },
-    {
-      id: 2,
-      type: "coin",
-      money: 50,
-      count: 12,
-    },
-  ],
-  machineMoney: [
-    { id: 1, money: 10, count: 0 },
-    { id: 2, money: 50, count: 0 },
-  ],
-  totalWalletMoney: 26_200,
-  machineTotalMoney: 0,
-};
+import MY_WALLET from "constants/myWallet";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "INIT":
       return action.data;
 
-    case "USE_COIN":
-      const coinState = state.map((coin) => {
-        return coin.id === action.targetId ? { ...coin, count: coin.count-- } : coin;
+    case "PUSH_COIN":
+      const decreasedCoins = state.map((coin) => {
+        return coin.id === action.targetId ? { ...coin, count: coin.count - 1 } : coin;
       });
 
-      return coinState;
+      return decreasedCoins;
 
     default:
       throw Error("WalletProvider action.type error");
@@ -45,30 +21,18 @@ const reducer = (state, action) => {
 
 const WalletProvider = ({ children }) => {
   const [wallet, dispatch] = useReducer(reducer, []);
-  const [coinInMachine, setCoinInMachine] = useState(moneyData);
-  const [coinSum, setCoinSum] = useState(0);
 
   const fetchMyWallet = () => {
     const initData = moneyData.map((moneyItem, index) => {
-      return { ...moneyItem, count: MY_MONEY[index].count };
+      return { ...moneyItem, count: MY_WALLET[index].count };
     });
 
     dispatch({ type: "INIT", data: initData });
   };
 
-  const setCoinWallet2Machine = (targetId) => {
-    const newData = coinInMachine.map((coinItem) =>
-      coinItem.id === targetId ? { ...coinItem, count: ++coinItem.count } : coinItem
-    );
-    setCoinInMachine(newData);
-  };
-
   const onPushCoin = useCallback((targetId) => {
-    dispatch({ type: "USE_COIN", targetId });
-    setCoinWallet2Machine(targetId);
+    dispatch({ type: "PUSH_COIN", targetId });
   }, []);
-
-  const walletvalues = { wallet, coinSum, coinInMachine };
 
   const dispatches = useMemo(() => {
     return {
@@ -80,12 +44,8 @@ const WalletProvider = ({ children }) => {
     fetchMyWallet();
   }, []);
 
-  useEffect(() => {
-    setCoinSum(calcTotalMoney(wallet));
-  }, [wallet]);
-
   return (
-    <WalletStateContext.Provider value={walletvalues}>
+    <WalletStateContext.Provider value={wallet}>
       <WalletDispatchContext.Provider value={dispatches}>{children}</WalletDispatchContext.Provider>
     </WalletStateContext.Provider>
   );
